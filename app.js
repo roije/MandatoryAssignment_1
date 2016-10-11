04f890dc697d206ea4ba8bb2d95a3bbcbaecc621
roije <roijellingsgaard@gmail.com>
2016-10-11 09:43:16 +0200

    done

--- app.js
+++ app.js
@@ -2,15 +2,26 @@ var express = require('express');
 var app = express();
 var fs = require('fs')
 
+//Home object.
+var home = require('./home.js')
+
 app.set('view engine', 'ejs');
 
+//Save object as JSON object
+var about = {
+    title: 'About',
+    subtitle: 'About Page for the Mandatory Assignment',
+    text: 'This text comes from a json file !! :)'
+}
+
+//Synchronously writing it to a .json file.
+var jsonObject = JSON.stringify(about, null, 4);
+fs.writeFileSync('./about.json', jsonObject, 'utf-8')
+
+    
 app.get('/', function(req, res){
 	//res.send('<h1>Hello again</h1>');
-    res.render('index', {
-               title: 'Home',
-                subtitle: 'Home Page for the mandatory assignment',
-                text: 'This is some text i chose to put in the paragraph'
-               });
+    res.render('index', home);
 }); 
 
 app.get('/about', function(req, res){
---
+++ home.js
@@ -0,0 +1,5 @@
+module.exports = {
+    title: 'Home',
+    subtitle: 'Home Page for the mandatory assignment',
+    text: 'This is some text i chose to put in the paragraph'
+}
